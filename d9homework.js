6ad1d94d612ba398b5cae588b4f2deedc8479c43
marios <mgeorgak859@gmail.com>
2022-05-05 15:52:07 +0300

    update added exercise two solution

--- d9homework.js
+++ d9homework.js
@@ -6,6 +6,18 @@ function area (l1, l2) {
     //console.log(rectangle)
 }
 
-area(50 , 50);
+area("hello" , "there");
 
 //Exercise 2
+
+function crazySum (n1,n2) {
+    let sumOfParameters = n1 + n2 
+    if (n1 === n2) {
+        //console.log(sumOfParameters * 3)
+    }
+    else {
+        //console.log(sumOfParameters)
+    }
+}
+
+crazySum (50,50)
