90fbcc288a0cc1187e8a83ffe826fca95fae8ebc
marios <mgeorgak859@gmail.com>
2022-05-05 15:23:22 +0300

    Create d9homework.js

---
+++ d9homework.js
@@ -0,0 +1,11 @@
+//Exercise 1
+
+function area (l1, l2) {
+    let rectangle = l1 * l2
+    //console.log("\n------The rectangle is------\n")
+    //console.log(rectangle)
+}
+
+area(50 , 50);
+
+//Exercise 2
